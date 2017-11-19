3c5fd4d5b453ca6d32f13f42747d875e0355441c
Shakti <shaktirp1@gmail.com>
2017-11-18 19:30:03 -0800

    added sendgrid email

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from datetime import datetime
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
-file = open('test.txt','a') 
-file.write('\n' + str(datetime.now())) 
+sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+from_email = Email("shaktirp1@gmail.com")
+to_email = Email("shaktirp1@gmail.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "Time is: " + str(datetime.now()))
+mail = Mail(from_email, subject, to_email, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
