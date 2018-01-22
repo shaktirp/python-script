28b1ffe5bb04d79c29ad2a3d9cb25580375acf04
Shakti <shaktirp1@gmail.com>
2018-01-22 13:13:01 -0800

    added to server

--- server.py
+++ server.py
@@ -1,5 +1,33 @@
+from twilio.rest import Client
+from time import gmtime, strftime
 from os import environ
-from flask import Flask
+try:
+    import env as _env
+    env = "dev"
+except Exception as e:
+    env = "prod"
 
-app = Flask(__name__)
-app.run(environ.get('PORT'))
+if env == "dev":
+    _SETTINGS = {
+        "SID": _env.SID,
+        "token": _env.token,
+        "to_number": _env.to_number,
+        "from_number": _env.from_number
+    }
+else:
+    _SETTINGS = {
+        "SID": environ.get('SID'),
+        "token": environ.get('token'),
+        "to_number": environ.get('to_number'),
+        "from_number": environ.get('from_number')
+    }
+
+client = Client(_SETTINGS["SID"], _SETTINGS["token"])
+
+body = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " - Hello from Python! "
+
+print(body, _SETTINGS)
+
+client.messages.create(to=_SETTINGS["to_number"],
+                       from_=_SETTINGS["from_number"],
+                       body=body)
